81787ccaee840ea5770b6565147ea3fd5ed57056
IMAD ALI <57463185+imad0653@users.noreply.github.com>
2021-04-12 03:34:21 +0500

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,7 @@
+seconds_calculation = 24 * 60 * 60
+
+print(f'There are {10 * seconds_calculation } seconds in ten days')
+print(f'There are {20 * seconds_calculation} seconds in twenty days')
+print(f'There are {30 * seconds_calculation} seconds in thirty days')
+print(f'There are {40 * seconds_calculation} seconds in fourty days')
+print(f'There are {50 * seconds_calculation} seconds in fifty days')
